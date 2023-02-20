a4059d84bea7c0a72f6d811ba893f6fde7ff4ddf
qiuye2015 <qiuye2015>
2023-02-20 13:30:48 +0800

    fix

--- init.lua
+++ init.lua
@@ -193,13 +193,14 @@ require('lazy').setup({
 -- See `:help vim.o`
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 
 -- Make line numbers default
 vim.wo.number = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+-- vim.o.mouse = 'a'
+vim.o.mouse = ''
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
