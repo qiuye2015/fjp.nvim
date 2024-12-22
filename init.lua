586e9367075128bd2f993ca465a19b52dea07d1b
anonymous <anonymous@users.noreply.github.com>
2024-12-22 18:07:14 +0800

    🚀 RELEASE:

--- init.lua
+++ init.lua
@@ -993,15 +993,15 @@ require('lazy').setup({
 -- FJP ADD
 require 'myself.basic'
 require 'myself.auto_runner'
-require('which-key').register {
-  -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]erm || [T]agbar', _ = 'which_key_ignore' },
-  ['<leader>n'] = { name = '[N]eotree', _ = 'which_key_ignore' },
-}
+-- require('which-key').register {
+--   -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+--   -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+--   -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+--   -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+--   -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+--   ['<leader>t'] = { name = '[T]erm || [T]agbar', _ = 'which_key_ignore' },
+--   ['<leader>n'] = { name = '[N]eotree', _ = 'which_key_ignore' },
+-- }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
